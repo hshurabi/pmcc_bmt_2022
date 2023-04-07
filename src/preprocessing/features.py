64de45713b8e99e977a937a2dad3798e30e9d912
Hamed <32173511+hshurabi@users.noreply.github.com>
2023-04-06 22:03:32 -0400

    Added get_correlated_cols function

--- src/preprocessing/features.py
+++ src/preprocessing/features.py
@@ -1,6 +1,31 @@
 
 from sklearn.feature_extraction.text import CountVectorizer
 
+def get_correlated_cols(local_df, **kwargs):
+
+  upper_limit = kwargs.get(upper_limit,1.0)
+  lower_limit = kwargs.get(lower_limit,0.75)
+  
+  corr = local_df.corr()
+  # Create correlation matrix
+  corr_matrix = corr.abs()
+
+  # Select upper triangle of correlation matrix
+  upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+
+  # Find features with correlation greater than 0.95
+  to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+
+  # Print correlated cols
+  s = upper.unstack()
+  so = s.sort_values(kind='quicksort')
+
+  print('Correlated columns are: ' )
+  print(so[(so>lower_limit)&(so<upper_limit)])
+  
+  return so[(so>lower_limit)&(so<upper_limit)]
+
+  
 def PrepareForMl(local_df):
   ''' function to make dummy variables.
   gets a dataframe as input
