a8609ac7f51d69af60feb5d5d359dd2c8bdb2501
Hamed <32173511+hshurabi@users.noreply.github.com>
2023-04-06 21:36:59 -0400

    Added remove_outliers function

--- src/preprocessing/cleaning.py
+++ src/preprocessing/cleaning.py
@@ -1,1 +1,21 @@
 
+def remove_outliers(data):
+  '''
+  Gets dataframe as input (with only categorical and numerical columns)
+  removes outliers, returns the updated dataframe.
+  Used IQR method to detect the outlieres
+  '''
+  data_Categorical = data.select_dtypes(include=['object'])
+  Categorical_Cols = data_Categorical.columns.tolist()
+  data_Numerical = data.drop(Categorical_Cols,axis=1)
+  Numerical_Cols = data_Numerical.columns.tolist()
+
+  Q1 = data[Numerical_Cols].quantile(0.25)
+  Q3 = data[Numerical_Cols].quantile(0.75)
+  IQR = Q3 - Q1
+  
+  
+  for col in Numerical_Cols:
+    data.loc[ ( (data[col] < (Q1[col] - 1.5 * IQR[col])) | (data[col] > (Q3[col] + 1.5 * IQR[col])) ),col] = np.nan
+    
+  return data
